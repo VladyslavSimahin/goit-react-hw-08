13c1f4a67a505ea596c124320f4957771deacd7c
VladyslavSimahin <vladyslav.simahin@gmail.com>
2024-04-12 18:50:17 -0600

    new

--- src/redux/filters/selectors.js
+++ src/redux/filters/selectors.js
@@ -1,15 +1,14 @@
 import { createSelector } from "@reduxjs/toolkit";
 import { selectContacts } from "../contacts/selectors";
-import { changeFilter } from "../filters/slice";
+
+export const selectNameFilter = (state) => state.filters.name;
 
 export const selectVisibleContacts = createSelector(
-  [selectContacts, changeFilter],
-  (contacts, textFilter) => {
+  [selectContacts, selectNameFilter],
+  (contacts, nameFilter) => {
     console.log("selectVisibleContacts");
-    return contacts.filter(
-      (contact) =>
-        contact.name &&
-        contact.name.toLowerCase().includes(textFilter.toLowerCase())
+    return contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(nameFilter.toLowerCase())
     );
   }
 );
